00bdf2f77160487e6fcc9c1266346ae2933edb5e
Matheus Amendoeira Ferraria <37910467+matheusaf@users.noreply.github.com>
2025-10-25 23:31:51 +0000

    adding executor

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@
 if [ ! -d "./env" ]; then
 	echo "env does not exist"
 	echo "creating env"
-	python3 -m venv env 
+	python3.11 -m venv env 
 	echo "env created"
 	echo ""
 fi
 
 source ./env/bin/activate
-python3 -m pip install pip --upgrade
-python3 -m pip install poetry
-python3 -m poetry install
+python3.11 -m pip install pip --upgrade
+python3.11 -m pip install poetry
+python3.11 -m poetry install
