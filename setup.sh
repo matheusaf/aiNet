3ae81a71d9af9f165d6004089dd2c0833ab3fcd5
Matheus Amendoeira Ferraria <matheusaf8@gmail.com>
2025-11-05 23:46:22 -0300

    updating pyproject

--- setup.sh
+++ setup.sh
@@ -1,14 +1,5 @@
 #!/bin/env bash
 
-if [ ! -d "./env" ]; then
-	echo "env does not exist"
-	echo "creating env"
-	python3.11 -m venv env 
-	echo "env created"
-	echo ""
-fi
-
-source ./env/bin/activate
-python3.11 -m pip install pip --upgrade
-python3.11 -m pip install poetry
-python3.11 -m poetry install
+python -m pip install pip --upgrade
+python -m pip install uv
+python -m uv sync
